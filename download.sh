0cbb472380f9d1453cba47003366b287bb625878
Michael B. Klein <mbklein@gmail.com>
2025-05-30 14:52:13 +0000

    Add If-Modified-Since logic to download script

--- download.sh
+++ download.sh
@@ -2,7 +2,28 @@
 dl() {
   local url=https://id.loc.gov/download/$1
   local dest=data/$1
-  curl -L -o $dest $url
+
+  mkdir -p "$(dirname "$dest")"
+
+  local ims=()
+  if [ -f "$dest" ]; then
+    # Get the file's last modified time in HTTP-date format
+    ims=(-H "If-Modified-Since: $(date -R -r "$dest")")
+  fi
+
+  # Download the file, save headers to a temp file
+  local headerfile
+  headerfile=$(mktemp)
+  curl -L -D "$headerfile" "${ims[@]}" -o "$dest" "$url"
+  
+  # Get Last-Modified header and set file mtime if present
+  local lm
+  lm=$(grep -i '^Last-Modified:' "$headerfile" | sed 's/Last-Modified: //I' | tr -d '\r')
+  if [ -n "$lm" ]; then
+    touch -d "$lm" "$dest"
+  fi
+
+  rm -f "$headerfile"
 }
 
 for t in \
